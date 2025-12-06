cc0047251afac95a3fd9343b0fc9bcaa2bb7b8d5
Maciej <maprz2017@gmail.com>
2025-12-06 16:59:05 +0100

    Aded new game confiramtion

--- ConductorSim/Assets/Scripts/MenusAndUI/MainMenuCotroller.cs
+++ ConductorSim/Assets/Scripts/MenusAndUI/MainMenuCotroller.cs
@@ -7,24 +7,36 @@ using UnityEditor;
 
 public class MainMenuCotroller : MonoBehaviour
 {
+    [SerializeField] GameObject newGameConfirm;
+    [SerializeField] GameObject credits;
+    [SerializeField] GameObject options;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        HideUIElement(newGameConfirm);
+        HideUIElement(credits);
+        HideUIElement(options);
+
         // Unlock and show cursor
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
     }
 
+    public void ShowUIElement(GameObject UIElement) { UIElement.SetActive(true); }
+
+    public void HideUIElement(GameObject UIElement) { UIElement.SetActive(false); }
+
     public void ContinueGame()
     {
         print("continue game");
-        SceneManager.LoadScene("GameScene");   
+        SceneManager.LoadScene("GameScene");
     }
 
     public void NewGame()
     {
         print("new game");
-        SceneManager.LoadScene("GameScene");   
+        SceneManager.LoadScene("GameScene");     
     }
 
     public void QuitGame()
