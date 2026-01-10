d6632821d7be295eccede51741f8d70692e9e7ed
Maciej <maprz2017@gmail.com>
2026-01-10 23:21:48 +0100

    Imported what could be imported

--- ConductorSim/Assets/Scripts/PlayerAndCamera/CameraController.cs
+++ ConductorSim/Assets/Scripts/PlayerAndCamera/CameraController.cs
@@ -4,17 +4,30 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] Transform startingRoom;
     [SerializeField] private float speed;
+    [SerializeField] private bool followPlayer = false; 
+    private Transform player; 
+
     private float currentPosX;
     private Vector3 velocity = Vector3.zero;
 
     void Start()
     {
+        if (player == null) { player = GameObject.FindGameObjectWithTag("Player")?.transform; }
+
         MoveToNewRoom(startingRoom);
     }
 
     private void Update()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+        float targetX = currentPosX;
+
+        if (followPlayer && player != null)
+        {
+            targetX = player.position.x;
+        }
+
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(targetX, transform.position.y, transform.position.z), ref velocity, speed);
+        currentPosX = transform.position.x;
     }
 
     public void MoveToNewRoom(Transform _newRoom)
